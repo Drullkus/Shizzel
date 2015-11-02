09279e3195ca1641676732c03b59b19ff3ceb0c4
Drullkus <ericsplanet@mac.com>
2015-11-02 15:53:16 -0800

    More things

--- src/main/java/us/drullk/shizzel/Shizzel.java
+++ src/main/java/us/drullk/shizzel/Shizzel.java
@@ -1,6 +1,5 @@
 package us.drullk.shizzel;
 
-import com.pahimar.ee3.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -8,6 +7,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import us.drullk.shizzel.proxy.CommonProxy;
 
 @Mod(modid = Shizzel.MOD_ID, name = Shizzel.MOD_NAME, version = Shizzel.VERSION, dependencies = "after:Chisel;after:Forestry")
 public class Shizzel {
@@ -22,26 +22,24 @@ public class Shizzel {
 	public static Shizzel instance;
 
 	public Shizzel() {
-		//ChiselAPIProps.MOD_ID = MOD_ID;
+		//Stuff
 	}
 
 	@SidedProxy(clientSide = "us.drullk.shizzel.proxy.ClientProxy", serverSide = "us.drullk.shizzel.proxy.CommonProxy")
 	public static CommonProxy proxy;
 
-
-
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		//proxy.preInit();
+		proxy.preInit();
 	}
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
-		//proxy.init();
+		proxy.init();
 	}
 
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		//proxy.postInit();
+		proxy.postInit();
 	}
 }
